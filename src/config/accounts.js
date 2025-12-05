90396b7f254a9bed6ad242b8e035f4a8db97c0e0
Tony <eternalShallow@163.com>
2025-12-05 22:14:04 +0800

    feat: Remove deprecated account configuration for LL-anti-martin

--- src/config/accounts.js
+++ src/config/accounts.js
@@ -251,28 +251,6 @@ export const ACCOUNT_CONFIGS = [
     updateTime: 0
   },
   {
-    id: 'll-anti-martin',
-    modelName: 'LL-anti-martin',
-    modelSlug: 'll-anti-martin',
-    modelColor: '#DC2626', // 深红色 (Red 600)
-    modelIcon: 'Rfg_logo.png',
-    uid: 'LL-anti-martin',
-    initialCapital: DEFAULT_INITIAL_CAPITAL,
-    description: 'An intelligent trading agent that interprets complex market structures and reacts dynamically to volatility.',
-    "user_address": "0x0000000000000000000000000000000000000000",
-    "signer_address": "0x0000000000000000000000000000000000000000",
-    enabled: true,
-    accountAlias: "RfgX",
-    asset: "USDT",
-    balance: "0.00000000",
-    crossWalletBalance: "0.00000000",
-    crossUnPnl: "0.00000000",
-    availableBalance: "0.00000000",
-    maxWithdrawAmount: "0.00000000",
-    marginAvailable: true,
-    updateTime: 0
-  },
-  {
     id: 'ds9-bn-new',
     modelName: 'DS9-BN-new',
     modelSlug: 'ds9-bn-new',
